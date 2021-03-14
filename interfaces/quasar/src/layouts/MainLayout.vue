3c9c2742720b0c94e0df09c329a081aa3bd5ad97
Shakthi Senavirathna <shakthis.mailbox@gmail.com>
2021-03-14 15:47:12 +0530

    Replaced interface using Quasar Framework

---
+++ interfaces/quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-avatar>
+          <q-img
+            src="icons/favicon-128x128.png"
+          />
+        </q-avatar>
+        <q-toolbar-title>
+          SLightString
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-ajax-bar ref="bar" position="top" color="orange" size="3px" />
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ interfaces/quasar/src/pages/Index.vue
@@ -0,0 +1,183 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="row q-pt-md">
+      <div class="col">
+        <q-select
+          v-model="fxEffect"
+          :options="fxEffects"
+          label="Effect"
+          outlined
+          :dropdown-icon="mdiMenuDown"
+          @input="setEffect()"
+        />
+      </div>
+    </div>
+    <div class="row q-pt-md">
+      <div class="col">
+        <q-select
+          v-model="fxAnimation"
+          :options="fxAnimations"
+          label="Animation"
+          outlined
+          :dropdown-icon="mdiMenuDown"
+          @input="setEffect()"
+        />
+      </div>
+    </div>
+    <div class="row q-pt-md">
+      <div class="col flex flex-center q-gutter-md">
+        <q-btn
+          stack
+          round
+          size="lg"
+          outline
+          color="blue"
+          :icon="mdiChevronDoubleLeft"
+          @click="setColor(-10)"
+        />
+        <q-btn
+          stack
+          round
+          size="lg"
+          outline
+          color="blue"
+          :icon="mdiChevronLeft"
+          @click="setColor(-1)"
+        />
+        <q-btn
+          stack
+          round
+          size="lg"
+          outline
+          color="blue"
+          :icon="mdiChevronRight"
+          @click="setColor(+1)"
+        />
+        <q-btn
+          stack
+          round
+          size="lg"
+          outline
+          color="blue"
+          :icon="mdiChevronDoubleRight"
+          @click="setColor(+10)"
+        />
+      </div>
+    </div>
+    <q-inner-loading :showing="connectionFailed" dark>
+      <q-spinner-gears size="50px" color="white" />
+    </q-inner-loading>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab :icon="mdiReload" color="blue" @click="fetchStatus()" />
+    </q-page-sticky>
+  </q-page>
+</template>
+
+<script>
+import {
+  mdiChevronDoubleLeft,
+  mdiChevronLeft,
+  mdiChevronRight,
+  mdiChevronDoubleRight,
+  mdiReload,
+  mdiMenuDown
+} from "@quasar/extras/mdi-v5";
+export default {
+  name: "Index",
+  data: () => ({
+    mdiChevronDoubleLeft,
+    mdiChevronLeft,
+    mdiChevronRight,
+    mdiChevronDoubleRight,
+    mdiReload,
+    mdiMenuDown,
+    fxEffects: [
+      "White",
+      "Color",
+      "Gradient",
+      "Gradient (Forward Animation)",
+      "Gradient (Backward Animation)",
+      "Rainbow",
+      "Rainbow (Forward Animation)",
+      "Rainbow (Backward Animation)"
+    ],
+    fxEffect: "",
+    fxAnimations: [
+      "None",
+      "Glitter",
+      "Breathing",
+      "Wipe Forward (Slow)",
+      "Wipe Backward (Slow)",
+      "Wipe Forward (Fast)",
+      "Wipe Backward (Fast)",
+      "Chaser Forward",
+      "Chaser Backward",
+      "Meteor Forward",
+      "Meteor Backward",
+      "Sinelon",
+      "Random Fade",
+      "Fire",
+      "Fire Colored"
+    ],
+    fxAnimation: "",
+    fxValue: 0,
+    overlay: false,
+    connectionFailed: false
+  }),
+  computed: {
+    fxEffectIndex: function() {
+      return this.fxEffects.indexOf(this.fxEffect);
+    },
+    fxAnimationIndex: function() {
+      return this.fxAnimations.indexOf(this.fxAnimation);
+    }
+  },
+  methods: {
+    fetchStatus: function() {
+      this.overlay = true;
+      this.$axios
+        .get("/api/status")
+        .then(response => {
+          let params = response.data.split(",");
+          this.fxEffect = this.fxEffects[params[0]];
+          this.fxAnimation = this.fxAnimations[params[1]];
+          this.fxValue = params[2];
+          this.overlay = false;
+        })
+        .catch(() => {
+          this.overlay = true;
+          this.connectionFailed = true;
+        });
+    },
+    setEffect: function() {
+      this.overlay = true;
+      this.$axios
+        .get(
+          `/api/set?fxeffect=${this.fxEffectIndex}&fxanimation=${this.fxAnimationIndex}`
+        )
+        .then(() => {
+          this.overlay = false;
+        })
+        .catch(() => {
+          this.overlay = true;
+          this.connectionFailed = true;
+        });
+    },
+    setColor: function(amount) {
+      this.overlay = true;
+      this.$axios
+        .get(`/api/set?fxcolor=${amount}`)
+        .then(() => {
+          this.overlay = false;
+        })
+        .catch(() => {
+          this.overlay = true;
+          this.connectionFailed = true;
+        });
+    }
+  },
+  created: function() {
+    this.fetchStatus();
+  }
+};
+</script>
